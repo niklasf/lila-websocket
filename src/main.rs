bef273fe02bfa504e21ddfe343d7464365c1fecf
Niklas Fiekas <niklas.fiekas@backscattering.de>
2019-06-29 17:21:45 +0200

    get session id from websocket handshake

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,28 @@
+use cookie::Cookie;
 use websocket::r#async::Server;
 
 use websocket::server::InvalidConnection;
+use websocket::header;
+use websocket::header::{Headers};
 
 use futures::{Future, Stream, future, stream};
+
+use std::str;
+use std::borrow::Cow;
+
+fn session_id(headers: &Headers) -> Option<String> {
+    let headers: &Vec<String> = headers.get::<header::Cookie>()?;
+
+    for header in headers {
+        let cookie = Cookie::parse(header).ok()?;
+        let (name, sid) = cookie.name_value();
+        if name == "lila2" {
+            return Some(sid.to_owned());
+        }
+    }
+
+    None
+}
 
 fn main() {
     let mut runtime = tokio::runtime::Builder::new().build().unwrap();
@@ -16,7 +36,9 @@ fn main() {
         .then(|r| future::ok(stream::iter_ok::<_, ()>(r)))
         .flatten()
         .for_each(move |(upgrade, addr)| {
+            dbg!(session_id(&upgrade.request.headers));
             dbg!(addr);
+            dbg!(&upgrade.request);
             upgrade.accept().and_then(|(s, _)| {
                 future::ok(())
             });
