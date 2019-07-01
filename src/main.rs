14fb018a1079862a1f177ce43eb709025a3ee84c
Niklas Fiekas <niklas.fiekas@backscattering.de>
2019-07-01 05:30:35 +0200

    read user id from mongodb

--- src/main.rs
+++ src/main.rs
@@ -6,34 +6,50 @@ use websocket::header;
 use websocket::header::{Headers};
 use websocket::message::OwnedMessage;
 
-use mongodb::ThreadedClient;
+use mongodb::ThreadedClient as _;
+use mongodb::db::ThreadedDatabase as _;
 
 use futures::{Future, Sink, Stream, future, stream};
+
+use serde::Deserialize;
 
 use std::str;
 use std::borrow::Cow;
 
-#[derive(Debug)]
-struct Sid(String);
+#[derive(Debug, Deserialize)]
+struct SessionCookie {
+    sessionId: String,
+}
 
-fn session_id(headers: &Headers) -> Option<Sid> {
+fn lila2(headers: &Headers) -> Option<String> {
     let headers: &Vec<String> = headers.get::<header::Cookie>()?;
 
     for header in headers {
         let cookie = Cookie::parse(header).ok()?;
         let (name, sid) = cookie.name_value();
         if name == "lila2" {
-            return Some(Sid(sid.to_owned()));
+            return Some(sid.to_owned());
         }
     }
 
     None
 }
 
-fn user_id(sid: &Sid) -> Option<String> {
-    let client = mongodb::Client::connect("127.0.0.1", 27017).unwrap();
+fn session_id(lila2: &str) -> Option<SessionCookie> {
+    serde_urlencoded::from_str(lila2).ok()
+}
 
-    None
+fn user_id(cookie: &SessionCookie) -> Option<String> {
+    let mut query = mongodb::Document::new();
+    query.insert("_id", &cookie.sessionId);
+
+    mongodb::Client::connect("127.0.0.1", 27017)
+        .expect("mongodb connection")
+        .db("lichess")
+        .collection("security")
+        .find_one(Some(query), None)
+        .expect("query by sid")
+        .and_then(|doc| doc.get_str("user").map(|s| s.to_owned()).ok())
 }
 
 fn main() {
@@ -48,19 +64,20 @@ fn main() {
         .then(|r| future::ok(stream::iter_ok::<_, ()>(r)))
         .flatten()
         .for_each(move |(upgrade, addr)| {
-            let sid = match session_id(&upgrade.request.headers) {
+            let sid = match lila2(&upgrade.request.headers).and_then(|l| session_id(&l)) {
                 Some(sid) => sid,
                 None => {
-                    upgrade.reject();
+                    upgrade.reject(); // TODO: await
                     return Ok(());
                 }
             };
 
+            dbg!(&sid);
             dbg!(user_id(&sid));
 
             let f = upgrade.accept().and_then(|(s, _)| {
                 let (mut sink, stream) = s.split();
-                sink.start_send(OwnedMessage::Text("foo".to_owned()));
+                sink.start_send(OwnedMessage::Text("foo".to_owned())); // TODO: await
 
                 stream
                     .map(|v| {
