367e8ee0d0f0c712c4c20e0d097d2b184796ed6d
Niklas Fiekas <niklas.fiekas@backscattering.de>
2019-07-06 00:23:46 +0200

    validate game ids

--- src/model.rs
+++ src/model.rs
@@ -2,21 +2,43 @@ use arrayvec::ArrayString;
 
 use std::str::FromStr;
 
-use serde::{Deserialize, Serialize};
+use serde::{Deserialize, Serialize, Serializer, Deserializer};
 
 /// An 8 charatcer game id.
-#[derive(Deserialize, Serialize, Eq, PartialEq, Hash, Clone, Debug)]
-#[serde(transparent)]
-pub struct GameId(pub ArrayString<[u8; 8]>);
+#[derive(Eq, PartialEq, Hash, Clone, Debug)]
+pub struct GameId(ArrayString<[u8; 8]>);
 
 #[derive(Debug)]
 pub struct InvalidGameId;
+
+impl GameId {
+    fn new(inner: ArrayString<[u8; 8]>) -> Result<GameId, InvalidGameId> {
+        if inner.chars().all(|c| c.is_ascii_alphanumeric()) && inner.len() == 8 {
+            Ok(GameId(inner))
+        } else {
+            Err(InvalidGameId)
+        }
+    }
+}
+
+impl Serialize for GameId {
+    fn serialize<S: Serializer>(&self, serializer: S) -> Result<S::Ok, S::Error> {
+        self.0.serialize(serializer)
+    }
+}
+
+impl<'de> Deserialize<'de> for GameId {
+    fn deserialize<D: Deserializer<'de>>(deserializer: D) -> Result<Self, D::Error> {
+        let inner = ArrayString::deserialize(deserializer)?;
+        GameId::new(inner).map_err(|_| serde::de::Error::custom("invalid game id"))
+    }
+}
 
 impl FromStr for GameId {
     type Err = InvalidGameId;
 
     fn from_str(s: &str) -> Result<GameId, InvalidGameId> {
-        Ok(GameId(ArrayString::from(s).map_err(|_| InvalidGameId)?))
+        GameId::new(ArrayString::from(s).map_err(|_| InvalidGameId)?)
     }
 }
 
