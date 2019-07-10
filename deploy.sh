94cd20a912327b0f1f312478850cedc4698219a2
Niklas Fiekas <niklas.fiekas@backscattering.de>
2019-07-10 16:55:18 +0200

    mv instead of rm during deploy

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -e
 cargo build --release
-ssh root@greco.lichess.ovh rm /usr/local/bin/lila-websocket
+ssh root@greco.lichess.ovh mv /usr/local/bin/lila-websocket /usr/local/bin/lila-websocket.bak
 scp ./target/release/lila-websocket root@greco.lichess.ovh:/usr/local/bin/lila-websocket
 ssh root@greco.lichess.ovh systemctl restart lila-websocket
